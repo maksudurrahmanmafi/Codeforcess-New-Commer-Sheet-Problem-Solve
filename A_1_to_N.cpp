c6d4b0404408fdfafd8a45a9577e94797684ceb0
Mafi2482 <maksudurrahmanmafi0@gmail.com>
2025-11-23 03:37:53 +0600

    1 to N

---
+++ A_1_to_N.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin>>n;
+    int i =1;
+    while(i<=n){
+        cout<<i<<'\n';
+        i++;
+    }
+    return 0;
+}
